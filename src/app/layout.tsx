1bfd049a2846f19bf144aeab1eba2f62ecca6674
Akash <armainuddinakash1@gmail.com>
2026-08-31 08:21:17 +0600

    fix: hide default Clerk header in app layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
         >
             <body className="min-h-full flex flex-col">
                 <ClerkProvider>
-                    <header className="flex justify-end items-center p-4 gap-4 h-16">
+                    {/* <header className="flex justify-end items-center p-4 gap-4 h-16">
                         <Show when="signed-out">
                             <SignInButton />
                             <SignUpButton>
@@ -48,7 +48,7 @@ export default function RootLayout({
                         <Show when="signed-in">
                             <UserButton />
                         </Show>
-                    </header>
+                    </header> */}
                     {children}
                 </ClerkProvider>
             </body>
