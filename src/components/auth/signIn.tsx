ecf89c50fd04bc82388d281fb58a2397a816b87b
Akash <armainuddinakash1@gmail.com>
2026-09-07 05:50:07 +0600

    use next link instead of anchor tag for forgot password link

--- src/components/auth/signIn.tsx
+++ src/components/auth/signIn.tsx
@@ -88,12 +88,12 @@ function SignInComponent() {
                     <div className="grid gap-2">
                         <div className="flex items-center">
                             <Label htmlFor="password">Password</Label>
-                            <a
+                            <Link
                                 href="/forgot-password"
                                 className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
                             >
                                 Forgot your password?
-                            </a>
+                            </Link>
                         </div>
                         <div className="relative">
                             <Input
