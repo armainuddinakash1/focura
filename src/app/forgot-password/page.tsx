08154c52940893cd4ba90a924d81105ddf5850c8
Akash <armainuddinakash1@gmail.com>
2026-09-07 05:41:49 +0600

    add forgot password page

---
+++ src/app/forgot-password/page.tsx
@@ -0,0 +1,11 @@
+import ForgotPasswordComponent from "@/components/auth/forgotPassword";
+
+function ForgotPasswordPage() {
+    return (
+        <div className="flex items-center justify-center min-h-screen bg-background">
+            <ForgotPasswordComponent />
+        </div>
+    );
+}
+
+export default ForgotPasswordPage;
